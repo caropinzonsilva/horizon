1c3c9c3a142d7be5b4681faeabdca0be039e29ab
Carolina Pinzon <c.pinzon543@uniandes.edu.co>
2017-02-10 17:17:01 -0500

    delete unused files

--- public/bower_components/angular-fusioncharts/app/js/app.js
+++
@@ -1,9 +0,0 @@
-angular.module('SampleApp', [ 'fusioncharts', 'ngRoute' ]).config(['$routeProvider', function($routeProvider) {
-  $routeProvider.
-      when('/', { templateUrl: 'app/partials/home.html', controller: HomeCtrl }).
-      when('/:action/', { templateUrl: 'app/partials/demo.html', controller: DemoCtrl }).
-      when('/:action/:name', { templateUrl: 'app/partials/demo.html', controller: DemoCtrl }).
-      otherwise({redirectTo: '/'});
-
-}]);
-
